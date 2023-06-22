209f956ba78bd3ef85c802b56bd869bc3895275c
Felipe1440 <135750035+Felipe1440@users.noreply.github.com>
2023-06-22 18:08:10 -0300

    sdf

--- index.ts
+++ index.ts
@@ -1,23 +1,18 @@
 async function main() {
-  const adapter = await navigator.gpu?.requestAdapter();
-  const device = await adapter?.requestDevice();
-  if (!device) {
-    fail('need a browser that supports WebGPU');
-    return;
-  }
+  const adapter = await navigator.gpu!.requestAdapter();
+  const device = await adapter!.requestDevice();
 
-  // Get a WebGPU context from the canvas and configure it
-  const canvas = document.querySelector('canvas');
-  const context = canvas.getContext('webgpu');
+  const canvas = document.querySelector("canvas");
+  const context = canvas.getContext("webgpu");
   const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
   context.configure({
     device,
     format: presentationFormat,
-      });
+  });
 
   const module = device.createShaderModule({
-    label: 'our hardcoded rgb triangle shaders',
-    code: `
+    label: "our hardcoded rgb triangle shaders",
+    code: /*wgsl*/`
       struct OurVertexShaderOutput {
         @builtin(position) position: vec4f,
         @location(0) color: vec4f,
@@ -50,57 +45,50 @@ async function main() {
   });
 
   const pipeline = device.createRenderPipeline({
-    label: 'hardcoded rgb triangle pipeline',
-    layout: 'auto',
+    label: "hardcoded rgb triangle pipeline",
+    layout: "auto",
     vertex: {
       module,
-      entryPoint: 'vs',
+      entryPoint: "vs",
     },
     fragment: {
       module,
-      entryPoint: 'fs',
+      entryPoint: "fs",
       targets: [{ format: presentationFormat }],
     },
   });
 
-  const renderPassDescriptor = {
-    label: 'our basic canvas renderPass',
-    colorAttachments: [
-      {
-        // view: <- to be filled out when we render
-        clearValue: [0.3, 0.3, 0.3, 1],
-        loadOp: 'clear',
-        storeOp: 'store',
-      },
-    ],
-  };
-
   function render() {
-    // Get the current texture from the canvas context and
-    // set it as the texture to render to.
-    renderPassDescriptor.colorAttachments[0].view =
-        context.getCurrentTexture().createView();
-
     const encoder = device.createCommandEncoder({
-      label: 'render triangle encoder',
+      label: "render triangle encoder",
     });
-    const pass = encoder.beginRenderPass(renderPassDescriptor);
+
+    const pass = encoder.beginRenderPass({
+      label: "our basic canvas renderPass",
+      colorAttachments: [
+        {
+          view: context.getCurrentTexture().createView(),
+          clearValue: [0.3, 0.3, 0.3, 1],
+          loadOp: "clear",
+          storeOp: "store",
+        },
+      ],
+    });
     pass.setPipeline(pipeline);
-    pass.draw(3);  // call our vertex shader 3 times
+    pass.draw(3); // call our vertex shader 3 times
     pass.end();
 
     const commandBuffer = encoder.finish();
     device.queue.submit([commandBuffer]);
   }
 
-  const observer = new ResizeObserver(entries => {
+  const observer = new ResizeObserver((entries) => {
     for (const entry of entries) {
       const canvas = entry.target as HTMLCanvasElement;
       const width = entry.contentBoxSize[0].inlineSize;
       const height = entry.contentBoxSize[0].blockSize;
       canvas.width = Math.min(width, device.limits.maxTextureDimension2D);
       canvas.height = Math.min(height, device.limits.maxTextureDimension2D);
-      // re-render
       render();
     }
   });
@@ -108,9 +96,7 @@ async function main() {
 }
 
 function fail(msg) {
-  // eslint-disable-next-line no-alert
   alert(msg);
 }
 
 main();
-  
